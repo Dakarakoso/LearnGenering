be7452296ba142fb426cf69a08620930c10f09ee
Willian Negishi <67539871+Dakarakoso@users.noreply.github.com>
2022-02-22 13:11:28 +0900

    Delete auth directory

--- auth/src/index.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { json } from "body-parser";
-const port = "3000";
-const app = express();
-app.use(json());
-
-app.listen(3000, () => {
-  console.log(`Listening on port ${port}, testing set up`);
-});
