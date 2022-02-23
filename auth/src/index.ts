95e16a283647e43bee30cd43e4c13b5a5e9883a6
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2022-02-23 16:59:30 +0900

    added setting to use google cloud

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const port = "3000";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("I like bananas");
+});
+
 app.listen(3000, () => {
   console.log(`Listening on port ${port}, testing set up`);
 });
