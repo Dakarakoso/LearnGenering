2bf89dc685d4213a8a63a7654f11ae8f23f875c7
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2022-02-25 06:48:07 +0900

    made the errors props are equal in order to client side be able to understand

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,18 @@
+export class DatabaseConnectionError extends Error {
+  statusCode = 500;
+  reason = "Error connecting to database";
+  constructor() {
+    super();
+
+    // Only because we are extending a built in class
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
 const port = "3000";
 const app = express();
 app.use(json());
@@ -11,6 +12,7 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log(`Listening on port ${port}, testing set up`);
