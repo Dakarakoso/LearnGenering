fd4eeb069334c3ebda5507230f2e0518ff31f20e
Willian Negishi <dakarakoso@Willians-MacBook-Pro.local>
2022-06-03 06:44:07 +0900

    test

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,7 +2,7 @@ import { natsWrapper } from "./nats-wrapper";
 import { OrderCreatedListener } from "./events/listener/order-created-listener";
 
 const start = async () => {
-  console.log("Starting....");
+  console.log("starting....");
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
